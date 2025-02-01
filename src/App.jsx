4994b3c101247e72d20935514a5017b26a620494
Gunjan7009 <gunjandixit1712@gmail.com>
2025-02-01 23:45:26 +0530

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,15 @@ import DataTable from './components/DataTable'
 import AnalyticTable from './components/AnalyticTable'
 import UserProfile from './components/UserProfile'
 
+
+const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem('token');
+  if (!token) {
+    return <Navigate to="/" replace />;
+  }
+  return children;
+};
+
 function App() {
   
 
@@ -18,7 +27,11 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/" element={<Login />} />
           {/* <Route path="*" element={<Register />} /> */}
-          <Route path="/dashboard/" element={<Dashboard />}>
+          <Route path="/dashboard/" element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }>
             <Route path="links" element={<DataTable />} />
             <Route path="analytics" element={<AnalyticTable />} />
             <Route path="settings" element={<UserProfile />} />
